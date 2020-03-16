e2988cab074991fe5c5db3a2eb3afd2cdd4b4714
Jiahao Li <andyofl1024@gmail.com>
2020-03-16 15:02:27 -0600

    uploaded lab1 python files

---
+++ lab1/lab1-Python3/Fraction/Fraction.py
@@ -0,0 +1,25 @@
+# Python3 program to reduce a fraction x/y 
+# to its lowest form 
+from math import gcd 
+
+# Function to reduce a fraction 
+# to its lowest form 
+def reduceFraction(x, y) : 
+	
+	d = gcd(x, y); 
+
+	x = x // d; 
+	y = y // d; 
+
+	print("x =", x, ", y =", y); 
+
+# Driver Code 
+if __name__ == "__main__" : 
+
+	x = 16; 
+	y = 10; 
+
+	reduceFraction(x, y); 
+
+# This code is contributed by Ryuga 
+
