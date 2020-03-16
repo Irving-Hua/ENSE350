494f7d389557ea0d24c02cec2db84e93061e2509
Jiahao Li <andyofl1024@gmail.com>
2020-03-16 14:52:51 -0600

    uploaded lab1 c++ files

---
+++ lab1/lab1-C++/Fraction/Fraction.cpp
@@ -0,0 +1,28 @@
+// C++ program to reduce a fraction x/y  
+// to its lowest form 
+  
+#include <iostream> 
+using namespace std; 
+  
+// Function to reduce a fraction to its lowest form 
+void reduceFraction(int x, int y) 
+{ 
+    int d; 
+    d = __gcd(x, y); 
+  
+    x = x / d; 
+    y = y / d; 
+  
+    cout << "x = " << x << ", y = " << y << endl; 
+} 
+  
+// Driver Code 
+int main() 
+{ 
+    int x = 16; 
+    int y = 10; 
+  
+    reduceFraction(x, y); 
+  
+    return 0; 
+} 
