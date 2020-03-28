111b147dad2aadbfceec7d530fe28557eb70c711
Jiahao Li <andyofl1024@gmail.com>
2020-03-27 23:06:59 -0600

    uploaded lab4 files

---
+++ lab4/README.md
@@ -0,0 +1,28 @@
+# Lab 2
+The RSA Cryptosystem programming
+
+### About
+```
+1. Using Newton-Raphson method to solve(less than 0.01% error): 
+```
+![alt-text](formula.png "formula")
+```
+2. Using (a) the Bisection method and (b) the Secant method to solve(less than 0.01% error): 
+f(x) = 230x4 + 18x3 + 9x2 - 221x -9
+```
+
+### Requirements
+- Features:
+```
+Compute given formula using different methods.
+The relative approximate error should be less than 0.01%.
+```
+- Inputs:
+```
+For 1: initial value for x.
+For 2: lower limit and upper limit number, first init value x_1 and second init value x_2
+```
+-Outputs:
+```
+For 1: iteration times, approximate value root and error
+For 2: iteration times, approximate value root and error
---
+++ lab4/lab4-Java/src/com/lab4/Client4.java
@@ -0,0 +1,75 @@
+package com.lab4;
+
+public class Client4 {
+    /**
+     * Main program entry point
+     *
+     * @param args String arguments from console (unused)
+     */
+    public static void main(String[] args) {
+        System.out.println("ENSE 350 Lab 4 - Daniel Shevtsov (SID: 200351253)");
+        System.out.println("\nTesting LU decomposition program with given matrix");
+        System.out.println("    3x_1  - 2x_2 + x_3  = -10");
+        System.out.println("    2x_1  + 6x_2 - 4x_3 =  44");
+        System.out.println("    -8x_1 - 2x_2 + 5x_3 = -26\n");
+        System.out.println("This equation is solved using LU decomposition following:");
+        System.out.println("    A * X = B   (A = coefficient matrix, X = solution vector, B = constants vector)");
+        System.out.println("    A = L * U   (decompose A into L and U)");
+        System.out.println("    B = L * Z   (solve for Z)");
+        System.out.println("    Z = U * X   (solve for X)");
+
+        System.out.println("\n**************************************************\n");
+
+        Matrix matrixA, matrixL, matrixU, matrixB, matrixZ, matrixX, matrixLU;
+
+        //Create a new matrix A that corresponds to the system of equations
+        double[][] a = {
+                {3, -2, 1},
+                {2, 6, -4},
+                {-8, -2, 5}
+        };
+        matrixA = new Matrix(a, 3, 3);
+
+        //Create the constants vector B
+        double[][] b = {{-10}, {44}, {-26}};
+        matrixB = new Matrix(b, 3, 1);
+
+        System.out.println("MATRIX A:");
+        Matrix.print(matrixA);
+        System.out.println("\nVECTOR B:");
+        Matrix.print(matrixB);
+
+        //Perform LU decomposition and extract L and U matrices from the returned LUDecomposition object
+        System.out.println("\nPerforming LU Decomposition...\n");
+        Matrix.LUDecomposition lu = matrixA.getLUDecomposition();
+        matrixL = lu.L;
+        matrixU = lu.U;
+
+        System.out.println("MATRIX L:");
+        Matrix.print(matrixL);
+        System.out.println("\nMATRIX U:");
+        Matrix.print(matrixU);
+
+        System.out.println("\nTesting that L * U = A");
+        System.out.println("\nMatrix L * U: ");
+        matrixLU = matrixL.multiply(matrixU);
+        Matrix.print(matrixLU);
+
+        System.out.println("\nL * U == A? " + (matrixLU.equals(matrixA) ? "TRUE" : "FALSE"));
+
+        System.out.println("\nSolving B = L * Z for Z");
+        matrixZ = Matrix.solveSystemByForwardSubstitution(matrixB, matrixL);
+
+        System.out.println("\nVECTOR Z:");
+        Matrix.print(matrixZ);
+
+        System.out.println("\nSolving Z = U * X for X");
+        matrixX = Matrix.solveSystemByBackwardSubstitution(matrixZ, matrixU);
+
+        System.out.println("\nVECTOR X (SOLUTIONS):");
+        Matrix.print(matrixX);
+
+        System.out.println("\nProgram completed successfully");
+    }
+
+}
